bf0950919a9509b4f227f1951be26a8deea0d1f1
Fidel Castro <fc.fidel1995@gmail.com>
2022-01-03 22:29:20 -0500

    done with the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
-import "./app.css";
+import "./App.css";
+import { useEffect, useMemo, useState } from "react";
+import Start from "./components/Start";
+import Timer from "./components/Timer";
 import Trivia from "./components/Trivia";
 
 function App() {
+  const [username, setUsername] = useState(null);
+  const [timeOut, setTimeOut] = useState(false);
   const [questionNumber, setQuestionNumber] = useState(1);
-  const [stop, setStop] = useState(false);
+  const [earned, setEarned] = useState("$ 0");
 
   const data = [
     {
@@ -75,54 +79,81 @@ function App() {
     },
   ];
 
-  const moneyPyramid = [
-    { id: 1, amount: "$ 100" },
-    { id: 2, amount: "$ 200" },
-    { id: 3, amount: "$ 300" },
-    { id: 4, amount: "$ 400" },
-    { id: 5, amount: "$ 1000" },
-    { id: 6, amount: "$ 2000" },
-    { id: 7, amount: "$ 4000" },
-    { id: 8, amount: "$ 8000" },
-    { id: 9, amount: "$ 16000" },
-    { id: 10, amount: "$ 32000" },
-    { id: 11, amount: "$ 64000" },
-    { id: 12, amount: "$ 125000" },
-    { id: 13, amount: "$ 250000" },
-    { id: 14, amount: "$ 500000" },
-    { id: 15, amount: "$ 1000000" },
-  ].reverse();
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "$ 100" },
+        { id: 2, amount: "$ 200" },
+        { id: 3, amount: "$ 300" },
+        { id: 4, amount: "$ 500" },
+        { id: 5, amount: "$ 1.000" },
+        { id: 6, amount: "$ 2.000" },
+        { id: 7, amount: "$ 4.000" },
+        { id: 8, amount: "$ 8.000" },
+        { id: 9, amount: "$ 16.000" },
+        { id: 10, amount: "$ 32.000" },
+        { id: 11, amount: "$ 64.000" },
+        { id: 12, amount: "$ 125.000" },
+        { id: 13, amount: "$ 250.000" },
+        { id: 14, amount: "$ 500.000" },
+        { id: 15, amount: "$ 1.000.000" },
+      ].reverse(),
+    []
+  );
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [questionNumber, moneyPyramid]);
+
   return (
     <div className="app">
-      <div className="main">
-        <div className="top">
-          <div className="timer">30</div>
-        </div>
-        <div className="bottom">
-          <Trivia
-            data={data}
-            setTimeOut={setStop}
-            setQuestionNumber={setQuestionNumber}
-            questionNumber={questionNumber}
-          />
-        </div>
-      </div>
-      <div className="pyramid">
-        <ul className="moneyList">
-          {moneyPyramid.map((m) => (
-            <li
-              className={
-                questionNumber === m.id
-                  ? "moneyListItem active"
-                  : "moneyListItem"
-              }
-            >
-              <span className="moneyListItemNumber">{m.id}</span>
-              <span className="moneyListItemAmount">{m.amount}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {!username ? (
+        <Start setUsername={setUsername} />
+      ) : (
+        <>
+          <div className="main">
+            {timeOut ? (
+              <h1 className="endText">You earned: {earned}</h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer
+                      setTimeOut={setTimeOut}
+                      questionNumber={questionNumber}
+                    />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Trivia
+                    data={data}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                    setTimeOut={setTimeOut}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((m) => (
+                <li
+                  className={
+                    questionNumber === m.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListItemNumber">{m.id}</span>
+                  <span className="moneyListItemAmount">{m.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,10 +1,23 @@
-import { useState,useEffect } from "react"
+import { useState,useEffect } from "react";
+import useSound from 'use-sound';
+
+import play from '../sounds/play.mp3';
+import wait from '../sounds/wait.mp3';
+import wrong from '../sounds/wrong.mp3';
+import correct from '../sounds/correct.mp3';
 
 function Trivia({data, setStop, questionNumber, setQuestionNumber}) {
 
     const [question, setQuestion] = useState(null);
     const [selectedAnswer, setSelectedAnswer] = useState(null);
     const [className, setClassName] = useState('answer');
+    const [letsplay] = useSound(play);
+    const [correctAnswer] = useSound(correct);
+    const [wrongAnswer] = useSound(wrong);
+
+    useEffect(()=>{
+        letsplay();
+    },[letsplay]);
 
     useEffect(() => {
         setQuestion(data[questionNumber - 1]);
@@ -20,12 +33,18 @@ function Trivia({data, setStop, questionNumber, setQuestionNumber}) {
         setSelectedAnswer(a);
         setClassName("answer active");
         delay(3000, () => setClassName(a.correct ? "answer correct" :  "answer wrong"));
-        delay(6000, () => {
+        delay(5000, () => {
             if(a.correct){
-                setQuestionNumber(prev => prev + 1);
-                setSelectedAnswer(null);
+                correctAnswer();
+                delay(1000, () => {
+                    setQuestionNumber(prev => prev + 1);
+                    setSelectedAnswer(null);
+                });
             } else {
-                setStop(true);
+                wrongAnswer();
+                delay(1000, () => {
+                    setStop(true);
+                });
             }
         });
     };
